ad4ecf55979cc8d2b0ad934e14f6d1bb7f1f6cdb
kayden-svg <kayden.sovann-bou@stu.ocsb.ca>
2026-09-24 11:14:46 -0400

    start

--- Main.py
+++ Main.py
@@ -6,5 +6,15 @@
 #
 # Project Description: See the README file
 #############################################
+from ctypes.wintypes import LPBOOL
+from winreg import FlushKey
 
 # THIS IS WHERE YOU CODE
+
+print("Irish")
+print("Ridgemont")
+print("Luke")
+print("Lesterb")
+print("Peter")
+print("Pias")
+print("Team name: Irish Wins: 4 Ties: 3 Losses: 1 Points: 11")
